b33731705954d96b1689d05328e0f547608dfc2c
Erinn Nelson <erinn@Erinns-MacBook-Pro.local>
2019-06-09 22:20:44 -0400

    game logic tuning and small css improvements

--- script.js
+++ script.js
@@ -5,11 +5,9 @@ const startButton = document.querySelector('button');
 // Cards used for the deck are specified here.
 // --NEED CODE FOR RANDOMIZING ? --
 const playingCards = `AS,AC,KH,KD,QS,QC,JH,JD,0S,0C,9H,9D,7S,7C,5H,5D,3S,3C,2H,2D`;
-const deckColors = ['assets/card_blue2.png', 'assets/card_red2.png', 'assets/card_black2.png']
-
-let colorAdvance = 0
-
-let deckColor = () => {
+const deckColors = ['assets/card_blue.png', 'assets/card_red.png', 'assets/card_black.png']
+let colorAdvance = Math.floor(Math.random() * 3);
+const deckColor = () => {
   if (colorAdvance === 0) {
     return deckColors[0];
   }
@@ -29,33 +27,57 @@ const drawNumber = `/draw/?count=20`;
 
 
 // Functions for flipping cards front or back
-let showCard = (cardDiv) => {
+const showCard = (cardDiv) => {
   cardDiv.firstElementChild.style.display = 'none';
   cardDiv.firstElementChild.nextElementSibling.style.display = '';
 };
 
-let hideCard = (cardDiv) => {
+const hideCard = (cardDiv) => {
   cardDiv.firstElementChild.style.display = '';
-  cardDiv.firstElementChild.nextElementSibling.style.display = 'none';
+  cardDiv.lastElementChild.style.display = 'none';
 }
+
+const incorrectCards = (cardDiv1, cardDiv2) => {
+  let cardDivs = gameBoard.children;
+  
+  // stops user from picking additional cards during timer
+  // --MAYBE INTERRUPT WITH ClICK INSTEAD--
+  for (i = 0; i < cardDivs.length; i += 1) {
+    cardDivs[i].classList.add('tempfixed');
+  }
+  cardDiv1.lastElementChild.style.borderColor = 'Red';
+  cardDiv2.lastElementChild.style.borderColor = 'Red';
+
+
+  setTimeout(function () {
+    hideCard(cardDiv1);
+    hideCard(cardDiv2);
+    cardDiv1.lastElementChild.style.borderColor = 'Black';
+    cardDiv2.lastElementChild.style.borderColor = 'Black';
+
+    // lets users click cards again
+    for (i = 0; i < cardDivs.length; i += 1) {
+      cardDivs[i].classList.remove('tempfixed');
+    }
+  }, 1000);
+};
 
 // Function that checks two selected cards to see if they are equal
 // If they are not equal the cards flip back around
 // --NEED CODE TO HIDE CARDS IF USER CLICKS A NEW CARD BEFORE THE PREVIOUS CARDS HIDE THEMSELVES--
-const checkCards = (card1, card2, div1, div2) => {
+const checkCards = (card1, card2, cardDiv1, cardDiv2) => {
   if (card1.value === card2.value) {
     console.log('match!')
-    div1.className = 'fixed';
-    div2.className = 'fixed';
-    console.log(div1);
-    console.log(div2);
+    cardDiv1.className = 'matched';
+    cardDiv2.className = 'matched';
+    cardDiv1.lastElementChild.style.borderColor = 'Lightgreen';
+    cardDiv2.lastElementChild.style.borderColor = 'Lightgreen';
+    console.log(cardDiv1);
+    console.log(cardDiv2);
 
   } else {
     console.log('try again, idiot');
-    setTimeout(function () {
-      hideCard(div1);
-      hideCard(div2);
-    }, 1500);
+    incorrectCards(cardDiv1, cardDiv2);
   }
 
 };
@@ -84,13 +106,13 @@ const buildBoard = async (deck) => {
     cardHolder.addEventListener('click', () => {
       // looked up some code from https://stackoverflow.com/questions/5898656/check-if-an-element-contains-a-class-in-javascript
       // This if statement checks to see if the card is already part of a match
-      if (cardHolder.classList.contains('fixed')) {
+      if (cardHolder.classList.contains('matched') || cardHolder.classList.contains('tempfixed')) {
         return;
       }
       if (cardsInPlay === 0) {
         card1 = card;
+        cardHolder.lastElementChild.style.borderColor = 'yellow';
         showCard(cardHolder);
-        console.log(cardHolder.firstElementChild);
         initialCardHolder = cardHolder;
         console.log(card.code);
         cardsInPlay = 1;
